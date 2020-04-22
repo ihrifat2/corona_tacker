784ea0b2c71137642cf558e5050ed6e8c965d0fe
Imran Hadid <imranhadid03@gmail.com>
2020-04-22 17:04:19 +0600

    Update CountryPicker.jsx

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core'
+import styles from './CountryPicker.module.css'
+import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+    const [fetchAllCountries, setFetchCountries] = useState([])
+    useEffect(() => {
+        const fetchCountry = async () => {
+            setFetchCountries(await fetchCountries())
+        }
+
+        fetchCountry();
+    }, [setFetchCountries])
     return (
-        <div>
-            <h1>CountryPicker</h1>
+        <div className={styles.formControl}>
+            <FormControl>
+                <NativeSelect defaultValue="" onChange={e => handleCountryChange(e.target.value)}>
+                    <option value="">Global</option>
+                    {
+                        fetchAllCountries.map((country, index) => <option value={country.name} key={index} data-thumbnail="https://www.countryflags.io/BD/shiny/64.png">{country.name}-{country.iso}</option>)
+                    }
+                </NativeSelect>
+            </FormControl>
         </div>
     );
 };
